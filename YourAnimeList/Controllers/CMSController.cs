9a93e090f14f3912acd10d87ba61f00f5edb1652
LabStudent-55-504266 <LabStudent-55-504266@ML-RefVm-41417>
2021-04-23 16:20:26 +0000

    Buidling CMS (testing inputdata)

---
+++ YourAnimeList/Controllers/CMSController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using YourAnimeList.Models;
+
+namespace YourAnimeList.Controllers
+{
+    public class CMSController : Controller
+    {
+
+        private readonly ILogger<CMSController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public CMSController(ILogger<CMSController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult AddAnime()
+        {
+            List<Anime> model = _context.Animes.ToList();
+
+            return View(model);
+        }
+
+        //GET 
+        public IActionResult CreateAnime()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateAnime(Anime obj)
+        {
+            _context.Animes.Add(obj);
+            _context.SaveChanges();
+            return RedirectToAction("AddAnime");
+        }
+    }
+}
--- YourAnimeList/Models/Anime.cs
+++ YourAnimeList/Models/Anime.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,19 +12,21 @@ namespace YourAnimeList.Models
         [Key]
         public int ID { get; set; }
 
+        [DisplayName("Anime Name")]
         public string AnimeName { get; set;}
-
+        [DisplayName("Genre")]
         public string AnimeGenere { get; set; }
-
+        [DisplayName("Description")]
         public string AnimeDescription { get; set; }
-
+        [DisplayName("Release Date")]
         public string AnimeReleaseDate { get; set; }
-        
+        [DisplayName("Sudio")]
         public string AnimeStudio { get; set; }
-
+        [DisplayName("Number of Episodes")]
         public int AnimeEpisodes { get; set; }
-
+        [DisplayName("Ratings")]
         public float AnimeRating { get; set; }
+        [DisplayName("Image Name.jpg")]
         public string AnimeURL { get; set; }
     }
 }
